61410c400951d84c529141c81f9b93a18afae315
Callan Bryant <callan.bryant@gmail.com>
2017-05-14 22:54:12 +0100

    jinja2 templates

--- dsportal/server.py
+++ dsportal/server.py
@@ -1,21 +1,24 @@
 from aiohttp import web
 import sys
 from os import path
+import jinja2
+import aiohttp_jinja2
 
 CONF_DIR = path.realpath(sys.argv[1])
-ASSET_DIR = join(CONF_DIR,'assets')
+ASSET_DIR = path.join(CONF_DIR,'assets')
 SCRIPT_DIR = path.dirname(path.realpath(__file__))
-STATIC_DIR = join(SCRIPT_DIR,'static')
-TEMPLATES_DIR = join(SCRIPT_DIR,'templates')
+STATIC_DIR = path.join(SCRIPT_DIR,'static')
+TEMPLATES_DIR = path.join(SCRIPT_DIR,'templates')
 
 app = web.Application(debug=True)
-app.router.add_static('/static',SCRIPT_DIR)
-app.router.add_static('/assets',ASSET_DIR)
-app.router.add_get('/',sso)
+app.router.add_static('/static',STATIC_DIR)
+#app.router.add_static('/assets',ASSET_DIR)
+#app.router.add_get('/',sso)
+
+aiohttp_jinja2.setup(app,loader=jinja2.FileSystemLoader(TEMPLATES_DIR))
 web.run_app(
         app,
-        port=int(PORT),
-        host='127.0.0.1',
+        port=int(8080),
         shutdown_timeout=6,
     )
 
--- setup_virtualenv.sh
+++ setup_virtualenv.sh
@@ -1,6 +1,6 @@
 #sudo apt-get install sudo apt-get install build-essential libssl-dev libffi-dev python3-dev
 
 cd $(dirname $0)
-virtualenv -v python3.5 venv
+virtualenv -p python3.6 venv
 source venv/bin/activate
 pip3 install -r requirements.txt
