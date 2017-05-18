3fd82c5563e39050ddf1ebc93d059b84497fac99
Callan Bryant <callan.bryant@gmail.com>
2017-05-18 23:15:15 +0100

    add about Entity

--- dsportal/scheduler.py
+++ dsportal/scheduler.py
@@ -3,7 +3,8 @@ class Scheduler(object):
     self.entities = list()
     self.healthchecks = list()
 
-    def __init__(self,entity_definitions):
-        for e in entity_definitions:
-            instance = 
+    def __init__(self):
 
+
+
+        def instantiate(definition):
