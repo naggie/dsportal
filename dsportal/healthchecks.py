582fc0f3872f1102866a2949b0f279215568f188
Callan Bryant <callan.bryant@gmail.com>
2017-05-24 22:29:47 +0100

    reformat method names + add broken_link_crawler

--- dsportal/healthchecks.py
+++ dsportal/healthchecks.py
@@ -2,7 +2,7 @@ from base import healthcheck
 from util import get_ups_data
 
 @healthcheck
-def ramusagecheck():
+def ramusage():
     "Checks RAM usage is less than 90%. Does not count cache and buffers."
     # http://www.linuxatemyram.com/
     with open('/proc/meminfo') as f:
@@ -25,7 +25,7 @@ def ramusagecheck():
 
 
 @healthcheck
-def cpuusagecheck():
+def cpu_usage():
     "Checks CPU load is nominal."
     #"return normalised % load (avg num of processes waiting per processor)"
     load = os.getloadavg()[0]
@@ -35,22 +35,22 @@ def cpuusagecheck():
 
 
 @healthcheck
-def diskusagecheck():
+def disk_usage():
     "Inspects used and available blocks on given mount points."
     s = statvfs(self.mountpoint)
     free = s.f_bsize * s.f_bavail
     total = s.f_bsize * s.f_blocks
 
 @healthcheck
-def ukmainsvoltagecheck():
+def mains_voltage(_min=216,_max=253):
     "Checks mains voltage falls within UK legal limits of 230V +10% -6%"
 
 @healthcheck
-def upsloadcheck():
+def ups_load():
     "Checks UPS is not overloaded"
 
 @healthcheck
-def batterylevelcheck():
+def battery_level():
     "Checks estimated time remaining and percentage"
 
     info = util.get_ups_data()
@@ -63,7 +63,7 @@ def batterylevelcheck():
 
 
 @healthcheck
-def uptimecheck():
+def uptime():
     "Specify uptime in days"
 
     with open('/proc/uptime', 'r') as f:
@@ -78,11 +78,11 @@ def uptimecheck():
 
 
 @healthcheck
-def cputemperaturecheck():
+def cpu_temperature():
     "Checks CPU Temperature is nominal"
 
 @healthcheck
-def gputemperaturecheck():
+def gpu_temperature():
     "Checks GPU Temperature is nominal"
 
     process = Popen(['nvidia-smi', '-q', '-d', 'TEMPERATURE'],
@@ -111,24 +111,28 @@ gputemperaturecheck.interval = 60
 
 
 @healthcheck
-def btrfspoolcheck():
+def btrfs_pool():
     "Checks BTRFS health"
 
 @healthcheck
-def httpstatuscheck():
+def http_status(url):
     "Checks service returns 200 OK"
 
 @healthcheck
-def certificateexpirycheck():
+def broken_links(url,ignore):
+    "Crawls website for broken links"
+
+@healthcheck
+def certificate_expiry():
     "Checks certificate isn't near expiry"
     # https://stackoverflow.com/questions/7689941/how-can-i-retrieve-the-tls-ssl-peer-certificate-of-a-remote-host-using-python
 
 @healthcheck
-def s3backupchecker():
+def s3_backup_checker():
     "Checks to see that a backup was made in the last 25 hours"
 
 @healthcheck
-def papouchth2e_temperature(url,
+def papouch_th2e_temperature(url,
         min_temp=10,
         max_temp=35,
         min_hum=20,
