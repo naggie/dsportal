3a771a5814a933f784fedd065c259bbbfbaae497
Callan Bryant <callan.bryant@gmail.com>
2017-05-30 22:51:11 +0100

    wording and test for healthy:False

--- dsportal/base.py
+++ dsportal/base.py
@@ -63,14 +63,13 @@ def healthcheckfn(fn):
             raise ValueError('Healthcheck result must be a dict')
 
         if 'healthy' not in result:
-            raise ValueError('Heathcheck result must have `healthy` key, a bool or None.')
+            raise ValueError('Heathcheck result must have `healthy` key: a bool or None.')
 
         if type(result['healthy']) != bool and result['healthy'] != None:
-            raise ValueError('''Heathcheck result must have `healthy` key, a
-                    bool or None. None means unknown-yet or not-applicable.''')
+            raise ValueError('`healthy` key must be bool or None. None means unknown-yet or not-applicable.')
 
-        if not result['healthy'] and not result['error_message']:
-            raise ValueError('error_message must be set if healthy is False or None')
+        if not result['healthy'] == False and not result['error_message']:
+            raise ValueError('error_message must be set if healthy is False')
 
         return result
 
