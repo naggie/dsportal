581f3595e3509cce053a7275e846d6453efd4924
Callan Bryant <callan.bryant@gmail.com>
2017-06-07 22:03:37 +0100

    add a sensible sounding UA to HTTPstatus check

--- dsportal/healthchecks.py
+++ dsportal/healthchecks.py
@@ -222,7 +222,13 @@ class HttpStatus(HealthCheck):
 
     @staticmethod
     def check(url,status_code=200,timeout=10):
-        r = requests.get(url,timeout=timeout)
+        r = requests.get(
+                url,
+                timeout=timeout,
+                headers={
+                    "User-Agent":'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+                    }
+                )
         if r.status_code != status_code:
             r.raise_for_status()
             raise Exception('Unexpected HTTP 200 received')
