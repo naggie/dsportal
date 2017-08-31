65f560580c02fe308c0c951274dd7929aeea5473
Callan Bryant <callan.bryant@gmail.com>
2017-08-31 22:18:44 +0100

    entry labels

--- installer/__init__.py
+++ installer/__init__.py
@@ -16,14 +16,15 @@ class Installer:
         self.master = master
         master.title("dsportal worker installer")
 
-        self.label = Label(master, text="Welcome to the dsportal worker windows service installer.")
-        self.label.grid(row=0,column=0)
+        Label(master, text="Welcome to the dsportal worker windows service installer.").grid(row=0,column=0)
 
+        Label(master, text="Server URL:").grid(row=1,column=0)
         self.url_entry = Entry()
-        self.url_entry.grid(row=1,column=0)
+        self.url_entry.grid(row=1,column=1)
 
+        Label(master, text="Server key:").grid(row=2,column=0)
         self.key_entry = Entry()
-        self.key_entry.grid(row=2,column=0)
+        self.key_entry.grid(row=2,column=1)
 
         self.install_button = Button(master, text="Install", command=self.greet)
         self.install_button.grid(row=3,column=0,sticky='E')
