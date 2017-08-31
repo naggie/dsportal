5903aaddd687467588bcbd77f1461f1fa021cfa9
Callan Bryant <callan.bryant@gmail.com>
2017-08-31 22:00:29 +0100

    padding

--- installer/__init__.py
+++ installer/__init__.py
@@ -17,13 +17,13 @@ class Installer:
         master.title("dsportal worker installer")
 
         self.label = Label(master, text="Welcome to the dsportal worker windows service installer.")
-        self.label.pack()
+        self.label.pack(pady=20,padx=10)
 
         self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack()
+        self.greet_button.pack(pady=20,padx=10)
 
         self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
+        self.close_button.pack(pady=20,padx=10)
 
     def greet(self):
         print("Greetings!")
